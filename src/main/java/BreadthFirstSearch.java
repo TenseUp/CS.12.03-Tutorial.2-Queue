57eee3aa1bc341a0bb3267faf5d647ca1c6167ce
SypherPK <46838766+TenseUp@users.noreply.github.com>
2024-02-29 09:38:46 +0800

    upload

--- src/main/java/BreadthFirstSearch.java
+++ src/main/java/BreadthFirstSearch.java
@@ -8,36 +8,28 @@ public class BreadthFirstSearch {
      */
     public static int[] bfs(int[][] graph) {
 
-        // Create a Queue and add the first node (0) to it. Use your Queue class that you just created!
+            CISQueue<Integer> queue = new CISQueue<>(0);
+            queue.enqueue(0);
 
-        // Create a visited array. This array will track whether we have visited a specific node.
+            boolean[] visited = new boolean[graph.length];
+            visited[0] = true;
 
-        // Create a bfs path array and a bfs path index. This array will track the order that the nodes were visited.
+            int[] bfsPath = new int[graph.length];
+            int bfsPathIndex = 0;
+            bfsPath[bfsPathIndex++] = 0;
 
-        // Add the first node (0) to the bfs array.
+            while (!queue.isEmpty()) {
+                int currentNode = queue.poll();
 
-        // Record the first node (0) as visited.
-        
-        // Time to traverse the graph! While the queue is not empty ...
+                for (int neighbor : graph[currentNode]) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        queue.enqueue(neighbor);
+                        bfsPath[bfsPathIndex++] = neighbor;
+                    }
+                }
+            }
 
-            // Dequeue (poll) the queue and store this value in a variable called currentNode.
-
-            // Record this node as visited.
-
-            // Add this node to the path and update the path index.
-
-            // Obtain an array of this node's neighbouring/adjacent nodes.
-        
-            // Explore the current nodes neighbouring nodes. For each neighbouring node ...
-
-                // If a neighbour hasn't been visited before ...
-
-                    // Record the neighbour as visited.
-        
-                    // Add the neighbour to the queue. 
-
-        // Return bfs path.
-        
-        return null;
-    }
+            return bfsPath;
+        }
 }
